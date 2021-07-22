db3fbcfeaf233e7820c15a89d4901f5f9b606b61
fm1539 <fm1539@nyu.edu>
2021-07-22 14:36:22 +0000

    using Giphy API with Flask

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from flask import Flask, render_template, request
 from datetime import datetime
 # from model import getImageUrlFrom
 import os
+import requests
 
 # -- Initialization section --
 app = Flask(__name__)
@@ -14,3 +15,10 @@ app = Flask(__name__)
 @app.route('/index')
 def index():
     return render_template("index.html", time = datetime.now())
+@app.route('/gif', methods=["GET", "POST"])
+def gif():
+    API_KEY = "MGBOzorZpu01A1c5pO1CH6bwi0RJmEOe"
+    URL = "http://api.giphy.com/v1/gifs/trending?api_key=" + API_KEY + "&limit=25"
+    results = requests.get(URL).json()
+    #return results
+    return render_template('results.html', results=results)	
